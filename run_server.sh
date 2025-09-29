b5a62850536baaf2decc4abaf3778763766f578e
Mr-Philo <1347549342@qq.com>
2025-09-29 09:04:14 +0000

    add notation

--- run_server.sh
+++ run_server.sh
@@ -1,1 +1,8 @@
+# To run Jekyll server locally, please ensure the envieonment is correctly set up.
+# Gemfile and Gemfile.lock should be carefully set. If you changed it, make sure the remote github workflow can run it correctly.
+# Pay attention to the version of jekyll and other dependencies, like Ruby, bundler, etc.
+# Ref:
+# https://cloud.tencent.com/developer/ask/sof/107035739
+# https://www.soinside.com/question/2MKZUtCmdH7D5U4EceQPbS
+
 bundle exec jekyll liveserve
